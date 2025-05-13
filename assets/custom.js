d577ca6593497078db6d8ac978bf56c69357c5ee
Lamis <lalhassanieh@borninteractive.com>
2025-05-13 16:34:18 +0300

    Refactor updateSlickDots function for improved readability and maintainability

--- assets/custom.js
+++ assets/custom.js
@@ -1,19 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("running custom.js");
-    function updateSlickDots() {
-        const leftSideMargin = document.querySelector('.slide-heading .top');
-        const leftOffset = leftSideMargin
-            ? leftSideMargin.getBoundingClientRect().left + window.scrollX
-            : 0;
-        if (window.innerWidth > 600) {
-            const leftSides = document.querySelectorAll('.slideshow-wrapper slick-dots');
-            leftSides.forEach(el => {
-                el.style.marginLeft = `${leftOffset}px`;
-            });
-        }
+function updateSlickDots() {
+    console.log('Updating slick dots');
+    const leftSideMargin = document.querySelector('.slide-heading .top');
+    const leftOffset = leftSideMargin
+        ? leftSideMargin.getBoundingClientRect().left + window.scrollX
+        : 0;
+    if (window.innerWidth > 600) {
+        const leftSides = document.querySelectorAll('.slideshow-wrapper slick-dots');
+        leftSides.forEach(el => {
+            el.style.marginLeft = `${leftOffset}px`;
+        });
     }
-    updateSlickDots();
-    // Recalculate on resize
-    window.addEventListener('resize', updateSlickDots);
-
-});
+}
+updateSlickDots();
+// Recalculate on resize
+window.addEventListener('resize', updateSlickDots);
+console.log('Updating slick dots');
