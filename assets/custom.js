6befaf77f2576aa3ba465a1b810d70f5541b0e8b
Lamis <lalhassanieh@borninteractive.com>
2025-05-13 17:20:12 +0300

    Refactor updateSlickDots function for improved readability and consistency

--- assets/custom.js
+++ assets/custom.js
@@ -1,35 +1,40 @@
 function updateSlickDots() {
-    console.log('Updating Slick dotss:::');
-    const leftSideMargin = document.querySelector('.slide-heading .top');
-    console.log('leftSideMargin:', leftSideMargin);
-    const leftOffset = leftSideMargin
-        ? leftSideMargin.getBoundingClientRect().left + window.scrollX
-        : 0;
-    console.log('2Left offset:', window.innerWidth);
-
-    const leftSides = document.querySelectorAll('.slick-dots');
-    console.log("leftSides", leftSides)
+  console.log('Updating slick dots:');
+  const leftSideMargin = document.querySelector('.slide-heading .top');
+  const leftOffset = leftSideMargin
+    ? leftSideMargin.getBoundingClientRect().left + window.scrollX
+    : 0;
+  console.log('Left offset:', window.innerWidth);
+  if (window.innerWidth > 600) {
+    const leftSides = document.querySelectorAll('.slideshow-wrapper .slick-dots');
     let firstVisibleElement = null;
 
     // Find the first visible element
     for (let i = 0; i < leftSides.length; i++) {
-        const element = leftSides[i];
-        if (element.offsetParent !== null) { // offsetParent is null if the element is hidden
-            firstVisibleElement = element;
-            break; // Exit the loop once the first visible element is found
-        }
+      const element = leftSides[i];
+      if (element.offsetParent !== null) { // offsetParent is null if the element is hidden
+        firstVisibleElement = element;
+        break; // Exit the loop once the first visible element is found
+      }
     }
 
     // Apply the margin to the first visible element, if found
     if (firstVisibleElement) {
-        firstVisibleElement.style.marginLeft = `${leftOffset}px`;
+      firstVisibleElement.style.marginLeft = `${leftOffset}px`;
     }
-
+  } else {
+        const leftSides = document.querySelectorAll('.slideshow-wrapper .slick-dots');
+        leftSides.forEach(el => {
+             el.style.marginLeft = `0px`;
+        });
+  }
 }
 
 // Call updateSlickDots after the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function () {
-    updateSlickDots();
-    // Recalculate on resize
-    window.addEventListener('resize', updateSlickDots);
+document.addEventListener('DOMContentLoaded', function() {
+  updateSlickDots();
+  //check again on load
+  setTimeout(updateSlickDots, 100);
+  // Recalculate on resize
+  window.addEventListener('resize', updateSlickDots);
 });
