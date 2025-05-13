6203183ec8487e339fa536771fd7d393f1c6cc93
Lamis <lalhassanieh@borninteractive.com>
2025-05-13 17:15:35 +0300

    Refactor updateSlickDots function for improved readability and consistency

--- assets/custom.js
+++ assets/custom.js
@@ -1,30 +1,30 @@
 function updateSlickDots() {
-    console.log('Updating slick dotss:');
+    console.log('Updating slick dotss::');
     const leftSideMargin = document.querySelector('.slide-heading .top');
-        console.log('leftSideMargin:', leftSideMargin);
+    console.log('leftSideMargin:', leftSideMargin);
     const leftOffset = leftSideMargin
         ? leftSideMargin.getBoundingClientRect().left + window.scrollX
         : 0;
     console.log('2Left offset:', window.innerWidth);
-    if (window.innerWidth > 600) {
-        const leftSides = document.querySelectorAll('.slick-dots');
-        console.log("leftSides", leftSides)
-        let firstVisibleElement = null;
 
-        // Find the first visible element
-        for (let i = 0; i < leftSides.length; i++) {
-            const element = leftSides[i];
-            if (element.offsetParent !== null) { // offsetParent is null if the element is hidden
-                firstVisibleElement = element;
-                break; // Exit the loop once the first visible element is found
-            }
-        }
+    const leftSides = document.querySelectorAll('.slick-dots');
+    console.log("leftSides", leftSides)
+    let firstVisibleElement = null;
 
-        // Apply the margin to the first visible element, if found
-        if (firstVisibleElement) {
-            firstVisibleElement.style.marginLeft = `${leftOffset}px`;
+    // Find the first visible element
+    for (let i = 0; i < leftSides.length; i++) {
+        const element = leftSides[i];
+        if (element.offsetParent !== null) { // offsetParent is null if the element is hidden
+            firstVisibleElement = element;
+            break; // Exit the loop once the first visible element is found
         }
     }
+
+    // Apply the margin to the first visible element, if found
+    if (firstVisibleElement) {
+        firstVisibleElement.style.marginLeft = `${leftOffset}px`;
+    }
+
 }
 
 // Call updateSlickDots after the DOM is fully loaded
