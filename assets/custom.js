3cdd149281f91c74744b1daf825b8bd651c6811b
Lamis <lalhassanieh@borninteractive.com>
2025-05-13 17:03:38 +0300

    Refactor updateSlickDots function to use correct selector for slick dots

--- assets/custom.js
+++ assets/custom.js
@@ -1,28 +1,29 @@
 function updateSlickDots() {
-  console.log('Updating slick dots');
-  const leftSideMargin = document.querySelector('.slide-heading .top');
-  const leftOffset = leftSideMargin
-    ? leftSideMargin.getBoundingClientRect().left + window.scrollX
-    : 0;
-  console.log('Left offset:', window.innerWidth);
-  if (window.innerWidth > 600) {
-    const leftSides = document.querySelectorAll('.slideshow-wrapper .slick-dots');
-    let firstVisibleElement = null;
+    console.log('Updating slick dots');
+    const leftSideMargin = document.querySelector('.slide-heading .top');
+    const leftOffset = leftSideMargin
+        ? leftSideMargin.getBoundingClientRect().left + window.scrollX
+        : 0;
+    console.log('Left offset:', window.innerWidth);
+    if (window.innerWidth > 600) {
+        const leftSides = document.querySelectorAll('.slick-dots');
+        console.log("leftSides", leftSides)
+        let firstVisibleElement = null;
 
-    // Find the first visible element
-    for (let i = 0; i < leftSides.length; i++) {
-      const element = leftSides[i];
-      if (element.offsetParent !== null) { // offsetParent is null if the element is hidden
-        firstVisibleElement = element;
-        break; // Exit the loop once the first visible element is found
-      }
-    }
+        // Find the first visible element
+        for (let i = 0; i < leftSides.length; i++) {
+            const element = leftSides[i];
+            if (element.offsetParent !== null) { // offsetParent is null if the element is hidden
+                firstVisibleElement = element;
+                break; // Exit the loop once the first visible element is found
+            }
+        }
 
-    // Apply the margin to the first visible element, if found
-    if (firstVisibleElement) {
-      firstVisibleElement.style.marginLeft = `${leftOffset}px`;
+        // Apply the margin to the first visible element, if found
+        if (firstVisibleElement) {
+            firstVisibleElement.style.marginLeft = `${leftOffset}px`;
+        }
     }
-  }
 }
 
 updateSlickDots();
